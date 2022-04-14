a4c98446ce5912faff36af576545f6e30c5bddcb
Piotr Orczykowski <orczykowski.peter@gmail.com>
2022-04-14 20:19:31 +0200

    Add endpoints for comments

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { PORT } = require('./configs/generalConfig')
 const postRouter = require('./routes/postRouter')
+const commentRouter = require('./routes/commentRouter')
 const app = express()
 
 //  db
@@ -11,6 +12,7 @@ app.use(express.json())
 
 //  routes
 app.use('/', postRouter)
+app.use('/', commentRouter)
 
 app.listen(PORT, () => {
     console.log(`Server is listening on http://localhost:${PORT}...`)
