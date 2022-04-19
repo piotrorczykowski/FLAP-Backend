6daab365a3709ffc16b46cc599dc3515ac9466a2
Piotr Orczykowski <orczykowski.peter@gmail.com>
2022-04-19 21:59:02 +0200

    Add user verification in post module

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -52,14 +52,14 @@ async function update(req, res) {
     }
 
     //  Check if every parameter exists
-    const { body, countOfPluses, countOfMinuses, countOfComments } = req.body
-    if(!body || countOfPluses === undefined || countOfMinuses === undefined || countOfComments === undefined) {
+    const { userId, body, countOfPluses, countOfMinuses, countOfComments } = req.body
+    if( !body || countOfPluses === undefined || countOfMinuses === undefined || countOfComments === undefined) {
         return res.status(400).json({ message: 'Not all parameters are given!' })
     }
 
     //  Try to update post of _id == postId
     try {
-        const post = await postService.updatePost(postId, body, countOfPluses, countOfMinuses, countOfComments)
+        const post = await postService.updatePost(postId, userId, body, countOfPluses, countOfMinuses, countOfComments)
         return res.status(201).send(post)
     } catch (err) {
         return res.status(500).json({ message: err.message })
@@ -74,9 +74,15 @@ async function remove(req, res) {
         return res.status(400).json({ message: 'Missing post id!' })
     }
 
+    //  Check if userId exists
+    const { userId } = req.body
+    if(!userId) {
+        return res.status(400).json({ message: 'Missing user id!' })
+    }
+
     //  Try to delete post of _id == postId
     try {
-        await postService.deletePost(postId)
+        await postService.deletePost(postId, userId)
         return res.sendStatus(204)
     } catch (err) {
         return res.status(500).json({ message: err.message })
